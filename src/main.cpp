1f6b5928ec65c25755ee93c242676daaa6d986d2
TRISTAN <debeaunetristan@gmail.com>
2023-02-15 16:18:38 +0100

    random rectangles moving

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
+#include <corecrt_math.h>
+#include <math.h>
 #include <stdlib.h>
+#include <vector>
+#include "glm/fwd.hpp"
 #include "p6/p6.h"
+
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
+
+// float angle(glm::vec2 v1, glm::vec2 v2)
+// {
+//     return 1f;
+// }
 
 int main(int argc, char* argv[])
 {
@@ -16,15 +26,37 @@ int main(int argc, char* argv[])
 
     // Actual app
     auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
+
+    std::vector<p6::Center> list_rect_pos(100);
+    std::vector<float>      list_mov_angle_dir(100);
+
+    // set up randoms values
+    for (unsigned int i = 0; i < 100; i++)
+    {
+        list_rect_pos[i]      = p6::Center{p6::random::number(-ctx.aspect_ratio(), ctx.aspect_ratio()), p6::random::number(-1, 1.)};
+        list_mov_angle_dir[i] = p6::random::number(0.f, 360.f);
+    }
+    float speed = 1.f;
+
     ctx.maximize_window();
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
         ctx.background(p6::NamedColor::Blue);
-        ctx.circle(
-            p6::Center{ctx.mouse()},
-            p6::Radius{0.2f}
-        );
+        for (unsigned int i = 0; i < 100; i++)
+        {
+            //  update the position:
+            list_rect_pos[i] = p6::Center{cos(list_mov_angle_dir[i]) * speed * ctx.delta_time() + list_rect_pos[i].value.x, sin(list_mov_angle_dir[i]) * speed * ctx.delta_time() + list_rect_pos[i].value.y};
+
+            // update the direction
+            // list_mov_angle_dir[i] =
+
+            // draw the square
+            ctx.square(
+                p6::Center{list_rect_pos[i]},
+                p6::Radius{0.2f}
+            );
+        }
     };
 
     // Should be done last. It starts the infinite loop.
